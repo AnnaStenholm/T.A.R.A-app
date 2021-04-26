541eb0bc5d39e5a5ebbd788571a11ef2f026ad01
Anna Stenholm <stenholm.a@gmail.com>
2021-04-26 17:13:22 +0200

    ändring boxar

--- src/KategoriVego.js
+++ src/KategoriVego.js
@@ -2,84 +2,96 @@ import React from 'react';
 import App from './App';
 import './kategori.css';
 
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function vego() {
-return<div>
+    return <div>
 
 
-    <div className="container">
-        <div className="row">
+        <div className="container">
+            <div className="row">
             <h3 className="kategori-titel">Vegetariska recept</h3>
-            <div className="container-rosa">
-                <div className="rosa-ruta">
-                    <div className="kategori-content">
-                        <div className="kategori-text"><a href="#"></a></div>
-                        <div className="kategori-img" src=""><a href="#">
-                        </a></div>
-                        <div className="kategori"><i>Vegetariskt</i></div>
-                        <i className="far fa-comments"></i>
-                        <div className="kategori-number">16</div>
-                    </div>
-                </div>
-                <div className="rosa-ruta">
-                    <div className="kategori-content">
-                        <div className="kategori-text"><a href="#"></a></div>
-                        <div className="kategori-img" src=""><a href="#">
-                        </a></div>
-                        <div className="kategori"><i>Vegetariskt</i></div>
-                        <i className="far fa-comments"></i>
-                        <div className="kategori-number">22</div>
-                    </div>
-                </div>
-                <div className="rosa-ruta">
-                    <div className="kategori-content">
-                        <div className="kategori-text"><a href="#"></a></div>
-                        <div className="kategori-img" src=""><a href="#">
-                        </a></div>
-                        <div className="kategori"><i>Vegetariskt</i></div>
-                        <i className="far fa-comments"></i>
-                        <div className="kategori-number">12</div>
-                    </div>
-                </div>
-                <div className="row-bottom">
-                    <div className="rosa-ruta">
-                        <div className="kategori-content">
-                            <div className="kategori-text"><a href="#"></a></div>
-                            <div className="kategori-img1" src=""><a href="#">
-                            </a></div>
-                            <div className="kategori"><i>Vegetariskt</i></div>
-                            <i className="far fa-comments"></i>
-                            <div className="kategori-number">15</div>
+                
+                    <div className="col">
+                        <div className="rosa-ruta">
+                            <div className="kategori-content">
+                                <div className="kategori-text"><a href="#"></a></div>
+                                <div className="kategori-img" src=""><a href="#">
+                                </a></div>
+                                <div className="kategori"><i>Vegetariskt</i></div>
+                                <i className="far fa-comments"></i>
+                                <div className="kategori-number">16</div>
+                            </div>
                         </div>
                     </div>
-
-                    <div className="rosa-ruta">
-                        <div className="kategori-content">
-                            <div className="kategori-text"><a href="#"></a></div>
-                            <div className="kategori-img" src=""><a href="#">
-                            </a></div>
-                            <div className="kategori"><i>Vegetariskt</i></div>
-                            <i className="far fa-comments"></i>
-                            <div className="kategori-number">13</div>
+                    <div className="col">
+                        <div className="rosa-ruta">
+                            <div className="kategori-content">
+                                <div className="kategori-text"><a href="#"></a></div>
+                                <div className="kategori-img" src=""><a href="#">
+                                </a></div>
+                                <div className="kategori"><i>Vegetariskt</i></div>
+                                <i className="far fa-comments"></i>
+                                <div className="kategori-number">22</div>
+                            </div>
                         </div>
                     </div>
-                    <div className="rosa-ruta">
-                        <div className="kategori-content">
-                            <div className="kategori-text"></div><a href="#"></a>
-                            <div className="kategori-img" src=""><a href="#">
-                            </a></div>
-                            <div className="kategori"><i>Vegetariskt</i></div>
-                            <i className="far fa-comments"></i>
-                            <div className="kategori-number">19</div>
+                    <div className="col">
+                        <div className="rosa-ruta">
+                            <div className="kategori-content">
+                                <div className="kategori-text"><a href="#"></a></div>
+                                <div className="kategori-img" src=""><a href="#">
+                                </a></div>
+                                <div className="kategori"><i>Vegetariskt</i></div>
+                                <i className="far fa-comments"></i>
+                                <div className="kategori-number">12</div>
+                            </div>
                         </div>
                     </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <div className="rosa-ruta">
+                                <div className="kategori-content">
+                                    <div className="kategori-text"><a href="#"></a></div>
+                                    <div className="kategori-img1" src=""><a href="#">
+                                    </a></div>
+                                    <div className="kategori"><i>Vegetariskt</i></div>
+                                    <i className="far fa-comments"></i>
+                                    <div className="kategori-number">15</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="rosa-ruta">
+                                <div className="kategori-content">
+                                    <div className="kategori-text"><a href="#"></a></div>
+                                    <div className="kategori-img" src=""><a href="#">
+                                    </a></div>
+                                    <div className="kategori"><i>Vegetariskt</i></div>
+                                    <i className="far fa-comments"></i>
+                                    <div className="kategori-number">13</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="rosa-ruta">
+                                <div className="kategori-content">
+                                    <div className="kategori-text"></div><a href="#"></a>
+                                    <div className="kategori-img" src=""><a href="#">
+                                    </a></div>
+                                    <div className="kategori"><i>Vegetariskt</i></div>
+                                    <i className="far fa-comments"></i>
+                                    <div className="kategori-number">19</div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    
                 </div>
-
-            </div>
-        </div>  
-        </div>
-        </div>
+           </div>
+       
+    
 
 
 }
